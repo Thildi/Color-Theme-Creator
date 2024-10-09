49a124fb14b83cb0c53398c736e4e125b91b1242
Marthilda <marthilda.vollmer@hotmail.de>
2024-10-09 11:45:58 +0200

    Add color to the theme functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import "./App.css";
+import ColorForm from "./Components/ColorForm/ColorForm";
+import { uid } from "uid";
+import { useState } from "react";
 
 function App() {
+  const [colors, setColors] = useState(initialColors);
+
+  const handleAddColor = (newColor) => {
+    const colorWithId = { ...newColor, id: uid() };
+    setColors([colorWithId, ...colors]);
+  };
+
   return (
     <>
       <h1>Theme Creator</h1>
 
-      {initialColors.map((color) => {
+      <ColorForm onSubmitColor={handleAddColor} />
+
+      {colors.map((color) => {
         return <Color key={color.id} color={color} />;
       })}
     </>
